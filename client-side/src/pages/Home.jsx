e6e1dbe967d656b4d143a639d13220300b5dff57
anhvunguyen-preciofishbone <vu.nguyen.anh@preciofishbone.se>
2023-09-15 15:23:46 +0700

    test

--- client-side/src/pages/Home.jsx
+++ client-side/src/pages/Home.jsx
@@ -25,6 +25,16 @@ const Home = () => {
                         <h1>Traveling opens the door to creating{" "}<span className="hightlight"> memories</span></h1>
                         <p></p>
                     </Col>
+                    <Col lg="6">
+                        <div className="hero__content">
+                            <div className="hero__subtitle d-flex align-items-center">
+                                <Subtitle subtitle={"Know Before You Go"} />
+                                <img src={worldImg} alt="" width={50} height={50} />
+                            </div>
+                        </div>
+                        <h1>Traveling opens the door to creating{" "}<span className="hightlight"> memories</span></h1>
+                        <p></p>
+                    </Col>
 
                     <Col lg="2">
                         <div className="hero__img-box">
